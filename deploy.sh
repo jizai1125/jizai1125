ab950daeca48f96fe4cf209311531fed7efcb7f1
jizai <1414589221@qq.com>
2021-02-07 17:00:45 +0800

    test2

--- deploy.sh
+++ deploy.sh
@@ -17,8 +17,8 @@ if [ -z "$GITHUB_TOKEN" ]; then
 else
   msg='来自github actions的自动部署'
   githubUrl=git@github.com:jizai1125/jizai1125.github.io.git
-  git config --global user.name "jizai1125"
-  git config --global user.email "1414589221@qq.com"
+  # git config --global user.name "jizai1125"
+  # git config --global user.email "1414589221@qq.com"
 fi
 git init
 git add -A
