6c887930fed7759c2360c12a9d32d74924907e2e
jizai <1414589221@qq.com>
2021-02-07 18:04:08 +0800

    5555555

--- deploy.sh
+++ deploy.sh
@@ -12,7 +12,7 @@ if [ -z "$GITHUB_TOKEN" ]; then
   githubUrl=git@github.com:jizai1125/jizai1125.github.io.git
 else
   msg='来自github actions的自动部署'
-  githubUrl=git@github.com:jizai1125/jizai1125.github.io.git
+  githubUrl=https://github.com/jizai1125/jizai1125.github.io.git
   git config --global user.name "jizai1125"
   git config --global user.email "1414589221@qq.com"
 fi
